1818582d0cb34b699c8b285f10b5515beb8213ae
= <=>
2013-09-25 21:40:49 +0000

    Fixed

--- spec/requests/users_pages_spec.rb
+++ spec/requests/users_pages_spec.rb
@@ -1,29 +1,5 @@
 require 'spec_helper'
 
-<<<<<<< HEAD
-describe "UsersPages" do
-  describe "Sign Up" do
-    let(:submit) { 'Create new account' }
-
-    before { visit signup_path }
-
-    describe "with invalid information" do
-      it "does not add the user to the system" do
-	expect { click_button submit }.not_to change(User, :count)
-      end
-    end
-
-    describe "with valid information" do
-      before do
-        fill_in 'Username', with: 'User Name'
-        fill_in 'Email', with: 'user@example.com'
-        fill_in 'Password', with: 'password'
-        fill_in 'Confirmation', with: 'password'
-      end
-
-      it "allows the user to fill in user fields" do
-        click_button submit
-=======
   describe "UsersPages" do
     describe "Sign Up" do
       it "allows the user to fill in name and password" do
@@ -40,7 +16,6 @@ describe "UsersPages" do
         it "does not add the user to the system" do
     expect { click_button submit }.not_to change(User, :count)
         end
->>>>>>> COS243
       end
   
       describe "with valid information" do
