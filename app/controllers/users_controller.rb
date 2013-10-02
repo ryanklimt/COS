19ee603a240c303b17aae43ccd3d88a14ea4089a
= <=>
2013-10-02 16:49:26 +0000

    Delete users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,6 +34,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end
+
   private
 
     def acceptable_params
