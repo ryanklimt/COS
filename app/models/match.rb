cf7b01300f9f0009f0c22c170044a151caa93b0e
= <=>
2013-10-30 17:42:12 +0000

    Delete extra tests

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,5 @@
 class Match < ActiveRecord::Base
   belongs_to :manager, polymorphic: true
+  has_many :player_matches
   has_many :players, through: :player_matches
 end
--- app/models/referee.rb
+++ app/models/referee.rb
@@ -1,5 +1,5 @@
 class Referee < ActiveRecord::Base
   belongs_to :user
-  has_many :contests
   has_many :matches, as: :manager
+  has_many :contests
 end
