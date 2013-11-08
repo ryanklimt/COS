e51127041a77d2d6b64a90754bd4e27eb7188c90
= <=>
2013-11-08 17:57:38 +0000

    Added referee adding and pages to edit/handle them

--- app/controllers/referees_controller.rb
+++ app/controllers/referees_controller.rb
@@ -1,35 +1,52 @@
 class RefereesController < ApplicationController
-  
-  #before_action :ensure_user_logged_in
-  
+  before_action :ensure_user_logged_in
+    
   def new
     @referee = current_user.referees.build
-  end
+  end 
   
   def create
     @referee = current_user.referees.build(acceptable_params)
+    if @referee.save then
+      flash[:success] = "Referee #{@referee.name} created!"
+      redirect_to @referee     
+    else
+      render 'new'
+    end
+  end
+  
+  def index
+    @referees = current_user.referees.all
   end
   
   def show
-  end
-  
-  def edit
+    @referee = Referee.find(params[:id])
   end
   
   def update
+    @referee = Referee.find(params[:id])
+    if @referee.update_attributes(acceptable_params)
+      flash[:success] = "Referee #{@referee.name} updated successfully!"
+      redirect_to @referee
+    else
+      render 'edit'
+    end
+  end
+  
+  def edit
+    @referee = Referee.find(params[:id])
   end
   
   def destroy
   end
   
   private
-    
     def acceptable_params
       params.require(:referee).permit(:name, :rules_url, :players_per_game, :upload)
     end
-  
+   
     def ensure_user_logged_in
-      redirect_to login_path, flash: { :warning => "Unable" } unless logged_in? 
+      redirect_to login_path, flash: { :warning => "Unable [not logged in]" } unless logged_in? 
     end
-  
+    
 end
--- app/models/referee.rb
+++ app/models/referee.rb
@@ -1,17 +1,16 @@
 class Referee < ActiveRecord::Base
-  
   belongs_to :user
+  has_many :contests
   has_many :matches, as: :manager
   
   def upload=(uploaded_io)
     if uploaded_io.nil?
-      # problem--deal with later
+      #problem--deal with later
     else
       time_no_spaces = Time.now.to_s.sub(/\s/, '_')
-      file_location = Rails.root.join('code', 'referees', time_no_spaces + current_user.id.to_s)
-      uploaded_file.read
+      file_location = Rails.root.join('code', 'referees', Rails.env, time_no_spaces).to_s
+      IO::copy_stream(uploaded_io, file_location)
+      self.file_location = file_location
     end
-    self.file_location = 'the final location on the server'
   end
-  
 end
