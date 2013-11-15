228c8f73f66cbe24205f05ca02ce0c88d7f3f91d
= <=>
2013-11-15 18:00:28 +0000

    Using validates_datetime to validate contest: passsing all tests

--- app/models/contest.rb
+++ app/models/contest.rb
@@ -1,18 +1,3 @@
-class MyValidator < ActiveModel::Validator
-  def validate(contest)
-    if contest.start && contest.deadline && contest.deadline > contest.start
-      contest.errors.add(:deadline, "Deadline cannot be after start")
-    end
-    if contest.start && contest.deadline && contest.deadline < DateTime.now.change(:usec => 0)
-      contest.errors.add(:deadline, "Deadline cannot be before now (present time)")
-    end 
-    if contest.start && contest.deadline && contest.start < DateTime.now.change(:usec => 0)
-      contest.errors.add(:start, "Start cannot be before now (present time)")
-    end
-  end
-
-end
-
 class Contest < ActiveRecord::Base
   belongs_to :user
   belongs_to :referee
@@ -27,7 +12,7 @@ class Contest < ActiveRecord::Base
   validates :deadline, presence: true
   validates :description, presence: true
   
-  include ActiveModel::Validations
-  validates_with MyValidator
+  validates_datetime :deadline, :on_or_after => lambda {Time.now.change(:usec => 0)}
+  validates_datetime :start, :on_or_after => lambda {:deadline}
   
 end
