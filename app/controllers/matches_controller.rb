70179d27ae3d1ada541ed4601fe714b229b4e7f0
= <=>
2013-12-06 17:50:30 +0000

    Modifications made in class...still passing all tests

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -2,11 +2,12 @@ class MatchesController < ApplicationController
 
   def index
     @contest = Contest.find(params[:contest_id])
-    @matches=Match.all
+    @matches = @contest.matches
+    #@matches = Match.all
   end
   
   def show
-    @match=Match.find(params[:id])
+    @match = Match.find(params[:id])
   end
   
 end
--- app/models/player_match.rb
+++ app/models/player_match.rb
@@ -3,4 +3,7 @@ class PlayerMatch < ActiveRecord::Base
   belongs_to :match
   validates :match, presence: true
   validates :player, presence: true
+  default_scope -> { order("player_matches.score DESC") }
+  scope :wins, -> { where(result: 'Win') }
+  scope :losses, -> { where(result: 'Loss') }
 end
